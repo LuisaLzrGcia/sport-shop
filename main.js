7376f91c4fe9f6c68134e404819683f051cd44ba
Alejandro <asotomiguel11@gmail.com>
2025-06-30 15:33:55 -0600

    Actualizacion

--- main.js
+++ main.js
@@ -1,95 +1,53 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navbar = `
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark py-2 mt-3 mb-3">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.html">
+                    <img src="logo.png" alt="logo" width="100">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenu">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-center" id="navbarMenu">
+                    <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="loNuevo.html">Lo Nuevo</a></li>
+                        <li class="nav-item"><a class="nav-link" href="hombre.html">Hombre</a></li>
+                        <li class="nav-item"><a class="nav-link" href="mujer.html">Mujer</a></li>
+                        <li class="nav-item"><a class="nav-link" href="nino.html">Niño/a</a></li>
+                        <li class="nav-item"><a class="nav-link text-danger" href="ofertas.html">Ofertas</a></li>
+                    </ul>
+                </div>
+                <form class="d-flex align-items-center">
+                    <input class="form-control form-control-sm me-2 d-none d-lg-block" type="search" placeholder="Buscar">
+                    <button class="btn btn-light btn-sm me-3 d-none d-lg-block" type="submit">
+                        <i class="bi bi-search"></i>
+                    </button>
+                    <a href="iniciarSesion.html" class="text-white me-3"><i class="bi bi-person" style="font-size: 1.2rem;"></i></a>
+                    <a href="favoritos.html" class="text-danger me-3"><i class="bi bi-heart" style="font-size: 1.2rem;"></i></a>
+                    <a href="carrito.html" class="text-white"><i class="bi bi-bag" style="font-size: 1.2rem;"></i></a>
+                </form>
+            </div>
+        </nav>
+    `;
+    document.getElementById('navbar').innerHTML = navbar;
+});
+const mensajes = [
+    { texto: "🚚 Envío gratis en compras mayores a $999", enlace: "index.html" },
+    { texto: "🔥 Unete para recibir informacion sobre productos exclusivos disponibles solo esta semana", enlace: "iniciarSesion.html" },
+    { texto: "🎁 Ofertas exclusivas de todos los productos", enlace: "ofertas.html" },
+];
 
-const productos = [
-    {
-        producto: "Camiseta",
-        precio: 899.00,
-        precioOferta: 0.00,
-        categoria: "Mujer" ,
-        tallas: ["S", "M", "L", "XL"],
-        imagen: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/11be4b02-6c3c-4db1-a1f9-83e5e18e9eac/W+J+SS+GF+GFX+TEE+MVP+J84.png",
-        descripcion: "Camiseta de algodón 100% con diseño moderno."
-    },
-       {
-        producto: "Camiseta",
-        precio: 759.00,
-        precioOferta: 0.00,
-        categoria: "Mujer",
-        tallas: ["S", "M", "L", "XL"],
-        imagen: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/55741066-c70a-4f8a-b403-5c52b81c4a97/W+NK+DF+TANK+BODYSUIT.png",
-        descripcion: "Camiseta de algodón 100% con diseño moderno."
-    },
-       {
-        producto: "Camiseta",
-        precio: 1239.00,
-        precioOferta: 0.00,
-        categoria: "Mujer",
-        tallas: ["S", "M", "L", "XL"],
-        imagen: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/78e27476-66fa-4374-9a50-bb4dca8e805e/W+ACG+DF+BABY+LUNGS+SS.png",
-        descripcion: "Camiseta de algodón 100% con diseño moderno."
-    },
-       {
-        producto: "Camiseta",
-        precio: 699.00,
-        precioOferta: 0.00,
-        categoria: "Mujer",
-        tallas: ["S", "M", "L", "XL"],
-        imagen: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/f7e49b98-638e-42e5-ae70-b05c520728c7/W+NK+ONE+FITTED+DF+SS+TOP+CPSL.png",
-        descripcion: "Camiseta de algodón 100% con diseño moderno."
-    },
-       {
-        producto: "Camiseta",
-        precio: 899.00,
-        precioOferta: 0.00,
-        categoria: "Mujer",
-        tallas: ["S", "M", "L", "XL"],
-        imagen: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/a0abc98b-a4c9-4148-8f3f-a6468e77d48e/W+NK+ARSWFT+DFADV+CROP+TANK.png",
-        descripcion: "Camiseta de algodón 100% con diseño moderno."
-    },
-       {
-        producto: "Camiseta",
-        precio: 849.00,
-        precioOferta: 0.00,
-        categoria: "Mujer",
-        tallas: ["S", "M", "L", "XL"],
-        imagen: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4a0bfd7d-834d-4bcb-a003-2797581fb72d/W+NK+ARSWFT+DFADV+SINGLET.png",
-        descripcion: "Camiseta de algodón 100% con diseño moderno."
-    },
-       {
-        producto: "Camiseta",
-        precio: 919.00,
-        precioOferta: 0.00,
-        categoria: "Mujer",
-        tallas: ["S", "M", "L", "XL"],
-        imagen: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/597ab33c-f5f7-4313-8020-50381ec13624/ENT+W+NK+DF+STAD+JSY+SS+AW.png",
-        descripcion: "Camiseta de algodón 100% con diseño moderno."
-    },
-       {
-        producto: "Camiseta",
-        precio: 899.00,
-        precioOferta: 0.00,
-        categoria: "Mujer",
-        tallas: ["S", "M", "L", "XL"],
-        imagen: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/b9323e8e-9871-478c-8730-94e36a49d6b4/W+NK+INF+DF+SS+TOP+WD+RIB.png",
-        descripcion: "Camiseta de algodón 100% con diseño moderno."
-    },
-       {
-        producto: "Camiseta",
-        precio: 1199.00,
-        precioOferta: 0.00,
-        categoria: "Mujer",
-        tallas: ["S", "M", "L", "XL"],
-        imagen: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/e3b17eb1-fdd1-450d-8c0b-ac1e47bc6fcb/FFF+W+NK+DF+STAD+JSY+SS+AW.png",
-        descripcion: "Camiseta de algodón 100% con diseño moderno."
-    },
-       {
-        producto: "Camiseta",
-        precio: 1049.00,
-        precioOferta: 0.00,
-        categoria: "Mujer",
-        tallas: ["S", "M", "L", "XL"],
-        imagen: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/2881f7b7-dc41-4ad7-b83e-5cc13c9a18a7/W+J+SS+OS+GFX+TEE+MIXTAPE.png",
-        descripcion: "Camiseta de algodón 100% con diseño moderno."
-    }
-    
-] 
+let indice = 0;
+const ticker = document.getElementById("mensaje-ticker");
+
+function mostrarMensaje() {
+    ticker.textContent = mensajes[indice].texto;
+    ticker.href = mensajes[indice].enlace;
+    indice = (indice + 1) % mensajes.length;
+}
+
+// Inicializa
+mostrarMensaje();
+// Cambia cada 5 segundos
+setInterval(mostrarMensaje, 5000);
 
