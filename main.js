eded529e4c6261bf45080274855315b99aa8e4e1
LuisaLzrGcia <maluisa.lzrgcia@gmail.com>
2025-06-30 10:51:40 -0600

    first commit

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const productos = [
     {
         producto: "Camiseta",
         precio: 20.00,
+        precioOferta: 15.00,
+        categoria: "Hombre",
         tallas: ["S", "M", "L", "XL"],
         imagen: "https://example.com/camiseta.jpg",
         descripcion: "Camiseta de algodón 100% con diseño moderno."
