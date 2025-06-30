ddd981d03e8fea08449e6a94023b1ca1a234abf9
JesusFranciscoCruz <cruz.ramirez.jesus.francisco@gmail.com>
2025-06-30 12:03:11 -0600

    Mis Cambios JesusFranciscoCruz

--- Niños/catalogo-niños.js
+++ Niños/catalogo-niños.js
@@ -1,7 +1,7 @@
 const productos = [
     {
         producto: "Playera",
-        precio: 20.00,
+        precio: 200,
         preciooferta: 0,
         tallas: ["S", "M", "L", "XL"],
         imagen: "https://m.media-amazon.com/images/I/41gy8zPjD+L._SY1000_.jpg",
@@ -10,7 +10,7 @@ const productos = [
 
     {
         producto: "Sudadera Champion color Azul",
-        precio: 20.00,
+        precio: 300,
         preciooferta: 0,
         tallas: ["S", "M", "L", "XL"],
         imagen: "https://www.championmexico.com.mx/cdn/shop/files/Sudadera-Nino-AzulRey-305903NU2_1.jpg?v=1711846983",
@@ -18,7 +18,7 @@ const productos = [
     },
      {
         producto: "Pantalon de pants",
-        precio: 20.00,
+        precio: 250,
         preciooferta: 0,
         tallas: ["S", "M", "L", "XL"],
         imagen: "https://ss433.liverpool.com.mx/xl/1072323251.jpg",
@@ -26,7 +26,7 @@ const productos = [
     },
      {
         producto: "Chamarra",
-        precio: 20.00,
+        precio: 350,
         preciooferta: 0,
         tallas: ["S", "M", "L", "XL"],
         imagen: "https://martimx.vtexassets.com/arquivos/ids/1273359/1127967182-1.png?v=638579683412830000",
@@ -34,7 +34,7 @@ const productos = [
     },
      {
         producto: "Short",
-        precio: 20.00,
+        precio: 200,
         preciooferta: 0,
         tallas: ["S", "M", "L", "XL"],
         imagen: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/d37e8015-79a1-45d4-a596-f6b89822d665/K+NK+DF+TRPHY23+SHORT+GX.png",
@@ -42,7 +42,7 @@ const productos = [
     },
      {
         producto: "Conjunto de color negro",
-        precio: 20.00,
+        precio: 400,
         preciooferta: 0,
         tallas: ["S", "M", "L", "XL"],
         imagen: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/cd5d36b5-31c7-40e0-b793-0aa854e5dbc1/sportswear-club-fleece-sudadera-con-capucha-nino-a-XxZcfQ.png",
@@ -50,7 +50,7 @@ const productos = [
     },
      {
         producto: "Conjunto de color verde",
-        precio: 20.00,
+        precio: 1200,
         preciooferta: 0,
         tallas: ["S", "M", "L", "XL"],
         imagen: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/d96fe8e4-ea6b-46af-be3a-fa6f6b09cf4f/sudadera-con-gorro-talla-grande-sportswear-club-fleece-twzNLC.png",
@@ -58,7 +58,7 @@ const productos = [
     },
      {
         producto: "Conjunto de color Negro con rojo",
-        precio: 20.00,
+        precio: 500,
         preciooferta: 0,
         tallas: ["S", "M", "L", "XL"],
         imagen: "https://surtidoradepartamental.vteximg.com.br/arquivos/ids/457787/180000098380_1.jpg?v=638250048912570000",
@@ -66,7 +66,7 @@ const productos = [
     },
      {
         producto: "Playera de futbol unisex",
-        precio: 20.00,
+        precio: 800,
         preciooferta: 0,
         tallas: ["S", "M", "L", "XL"],
         imagen: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/597f53d5-d9a7-4ef3-8426-e5e5ae4543c9/CA+Y+NK+DF+JSYSS+STAD+HM.png",
@@ -74,7 +74,7 @@ const productos = [
     },
      {
         producto: "Conjunto de futbol Niño/a",
-        precio: 20.00,
+        precio: 1200,
         preciooferta: 0,
         tallas: ["S", "M", "L", "XL"],
         imagen: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/61cd991b-c65d-4efc-8998-073b7b9cc1aa/CFC+Y+NK+DF+JSY+SS+STAD+HM.png",
